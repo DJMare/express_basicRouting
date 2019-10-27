9ddd4f5698a9741a10117e1b7fa00d4586ec1a78
Mariela Enciso <35668707+DJMare@users.noreply.github.com>
2019-10-26 20:32:37 -0700

    Add files via upload

---
+++ models/users.js
@@ -0,0 +1,32 @@
+module.exports.people = [
+    {
+      id: 3412,
+      name: 'Walter White',
+      username: 'heisenberg',
+      age: 52
+    },
+    {
+      id: 5329,
+      name: 'Skylar White',
+      username: 'yoko.ono',
+      age: 40
+    },
+    {
+      id: 9384,
+      name: 'Jesse Pinkman',
+      username: 'captainAmerica',
+      age: 25
+    },
+    {
+      id: 2930,
+      name: 'Hank Schrader',
+      username: 'hsdea66',
+      age: 43
+    },
+    {
+      id: 5723,
+      name: 'James McGill',
+      username: 'saul.goodman',
+      age: 48
+    }
+  ];
